683fb091c030987b03dea6e170ee5b2ac1b2f3e9
Garrett McDonnell <ggm6@zips.uakron.edu>
2018-02-15 11:19:16 -0500

    Updating card.hpp

--- Euchre/card.hpp
+++ Euchre/card.hpp
@@ -20,12 +20,25 @@ Hearts
 class Card
 {
 public:
-Card() : rank(Ace),suit(Spades) {}
-Card(Rank r, Suit s) : rank(r), suit(s) {}
+Card() = default;
+Card(Rank r, Suit s) : bits ((unsigned)s << 3 | unsigned(r)) {}
+bool operator == (Card c) const {return bits==c.bits;}
+bool operator != (Card c) const {return bits!=c.bits;}
+void operator = (Card c) {bits=c.bits;}
+bool operator < (Card c) const {return bits<c.bits;}
+bool operator > (Card c) const {return bits >c.bits;}
+~Card() {bits=0;}
+friend bool operator == (Card a, Card b);
 
 private:
-Rank rank;
-Suit suit;
+Rank rank:3;
+Suit suit:3;
+unsigned char bits:3;
 };
 
+
+bool operator == (Card a, Card b)
+{
+	return a.bits==b.bits;
+}
 #endif // CARD_HPP_INCLUDED
